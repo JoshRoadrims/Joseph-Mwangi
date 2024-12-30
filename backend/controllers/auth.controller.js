222ce290321a6eb34083bbdaf8b9cc8f94de82ff
Joeeazy <joeeazy34@gmail.com>
2024-12-30 23:32:04 +0300

    auth controller functions updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,116 +1,207 @@
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
-import transporter from "../configs/nodemailer.js";
-import userModel from "../models/user.model.js";
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
-export const register = async(req, res) => {
-    //get name, email & passwordd from the body
-    const {name, email, password} = req.body;
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendPasswordResetEmail, sendResetSuccessEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
-    //check for details
-    if(!name || !email || !password){
-        return res.json({success: false, message: "missing Details"})
-    }
-
-    //if all details exist
+export const signup = async (req, res) => {
     try {
-        //store details on our db
-        
-        //first check if User exists in db
-        const existingUser = await userModel.findOne({email})
-        if(existingUser) {
-            return res.json({sucess: false, message: "User already exists"})
+        const {name, email, password} = req.body;
+
+        //check for neccessary inputs
+        if(!name, !email, !password) {
+            return res.status(400).json({success: false, message: "All fields are required"})
         }
+    
+        const existingUser = await User.findOne({email});
+        if(existingUser) {
+            return res.status(400).json({success: false, message: "User Already exists"})
+        }
+    
+        //hashpassword before storing user to db
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        //  encrypt password before storing using bcryptjs
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
 
-        // creATE new USER
-        const user = new userModel({name, email, password: hashedPassword})
+        //create the new user
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 //24hours
+        })
+        //save user to db
+        await user.save()
 
-        //save user in the db
-        await user.save();
+        // jwt token
+        generateTokenAndSetCookie(res, user._id)
 
-        //generate tokens 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d"})
+        //send verification email
+        await sendVerificationEmail(user.email, verificationToken)
 
-        //send token to user inform of cookie
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000 //7days in milliseconds
+        return res.status(201).json({success:true, message: "User Created successfully",
+        user: {
+            ...user._doc,
+            password: undefined,
+        }
+    })
+    
+    } catch (error) {
+        res.status(400).json({success: false, message: error.message})
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    //user inputs verification code 123456
+    const {code} = req.body;
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() }
         })
 
-        //send a welcome email
-        const mailOptions = {
-            from: process.env.SENDER_EMAIL,
-            to: email,
-            subject: "Welcome to Roadrims Logistics",
-            text: `Welcome to Roadrims Logistics Your Trusted Logistics and Transport partner. Your account has been created with email id: ${email}`
+        if(!user) {
+            return res.status(400).json({success: false, message: "Invalid or exipired verification code"})
         }
 
-        await transporter.sendMail(mailOptions);
+        user.isVerified =  true;
 
-        return res.status(200).json({sucess:true});
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save();
+
+        await sendWelcomeEmail(user.email, user.name)
+
+        res.status(200).json({success: true, message: "Email verified successfully", user: {...user._doc, password: undefined}})
 
     } catch (error) {
-        res.status(404).json({success: false, message: error.message})
+        console.log("Error in verifyEmail", error)
+        res.status(400).json({success: false, message: error.message})
     }
+
 }
 
 
 export const login = async (req, res) => {
+    //get user input
     const {email, password} = req.body;
-
-    // validate userinput
-    if(!email || !password) {
-        return res.json({success: false, message: "email and password are required"})
+    if(!email || !password){
+        return res.status(404).json({success: false, message: "Enter Email and Password"})
     }
 
     try {
-        //check if user exists
-        const user = await userModel.findOne({email})
-        if(!user){
-            return res.json({success: false, message: "Invalid email"})
+        //check if user is registered
+        const user = await User.findOne({email});
+
+        if (!user) {
+            return res.status(404).json({success: false, message: "User does not exist"})
         }
 
-        //check if password entered is correct
-        const isMatch = await bcrypt.compare(password, user.password)
+        //compare password
+        const comparePassword = await bcrypt.compare(password, user.password)
 
-        if(!isMatch){
-            return res.json({success: false, message: "Inavlid Password"})
+        if(!comparePassword){
+            return res.status(404).json({success: false, message: "Wrong Password"})
         }
 
-        //correct password ? => generate token
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d"})
-        //send cookie as a response
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7days to milliseconds
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date()
+
+        await user.save();
+
+        return res.status(200).json({success:true, 
+            message: "Login successful", 
+            user: {
+                ...user._doc, 
+                password: undefined,
+            }
         })
-
-        return res.status(200).json({sucess:true});
-
     } catch (error) {
-        return res.status(404).json({success: false, message: error.message})
+        console.log(`error in Login functon`, error)
+        res.status(400).json({success: false, message: error.message})
     }
 }
 
-
 export const logout = async (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({success: true, message: "User logged out successfully"})
+}
+
+export const forgotPassword = async (req, res) => {
+    const {email} = req.body;
     try {
-        //clear user's cookie from response
-        res.clearCookie("token", {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
+        const user = await User.findOne({email})
+        if(!user) {
+            return res.status(400).json({success: false, message: "Invalid email"})
+        }
+
+        //generate reset token
+        const resetToken = crypto.randomBytes(20).toString("hex");
+        const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000 //1hour
+
+        //save to db
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpiresAt = resetTokenExpiresAt;
+        await user.save();
+
         
-        return res.status(200).json({sucess: true, message: "logged Out"})
+        await sendPasswordResetEmail(user.email, `${process.env.CLIENT_URL}/reset-password/${resetToken}}`)
+
+        res.status(200).json({success:true, message: "Password reset Link sent to your email"})
     } catch (error) {
-        return res.status(404).json({success: false, message: error.message})
+        res.status(400).json({success: false, message: error.message})
+    }
+}
+
+export const resetPassword = async (req, res) => {
+    try {
+        const {token} = req.params;
+
+        const {password} = req.body;
+
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpiresAt: { $gt: Date.now() },
+        })
+
+        if(!user) {
+            return res.status(400).json({success: false, message: "Invalid or expired reset Token"})
+        }
+
+        //update password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // update db
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpiresAt = undefined
+
+        //save to db
+        await user.save();
+
+        await sendResetSuccessEmail(user.email);
+
+        res.status(200).json({success: true, message:"Password reset Successful"})
+
+    } catch (error) {
+        console.log("Error in resetPassword", error)
+        res.status(400).json({success: false, message: error.message})
+    }
+}
+
+export const checkAuth = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId).select("-password");
+        if(!user) {
+            return res.status(400).json({success: false, message: "User not found"})
+        }
+
+        return res.status(200).json({success: true, user})
+    } catch (error) {
+        console.log(`Error in checkAuth`, error)
+        res.status(500).json({success: false, message: "Server error"})
     }
 }
